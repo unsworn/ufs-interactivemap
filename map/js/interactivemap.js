7280e631229ae2d8705170cf72d039ab5ee8df73
Sveta Suvorina <sveta@unswornindustries.org>
2015-01-29 16:37:38 +0100

    first prototype

---
+++ map/js/interactivemap.js
@@ -0,0 +1,33 @@
+angular.module("interactivemap", [])
+
+.controller("SlideshowCtrl", function($http) {
+  var self = this;
+  this.entries = [];
+  this.slideShows = [];
+  var current = 0;
+  
+  this.change = function() {
+    if (current === 0) {
+      this.entries = this.slideShows[1];
+      current = 1;
+    } else {
+      this.entries = this.slideShows[0];
+      current = 0;
+    }
+  };
+
+  function onSuccess(csvData, status, headers, config) {
+    // Convert CSV to JSON.
+    var jsonData = Papa.parse(csvData, {header: true});
+    self.slideShows.push(jsonData.data);
+    console.log('entries after http', self.entries);
+  }
+
+  // Load CSV data.
+  $http.get('/data/data1.csv')
+    .success(onSuccess);
+
+  $http.get('/data/data2.csv')
+    .success(onSuccess);
+
+});
